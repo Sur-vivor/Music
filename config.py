333f0cffdd3b149e8c115750252f7d1989497d5c
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-07-20 12:24:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,8 @@ class Config:
     ADMIN = os.environ.get("ADMINS", '')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", ''))
-    CHAT = set(int(x) for x in os.environ.get("CHAT", "").split())
+    CHAT = os.environ.get("CHATS", "")
+    CHATS = [int(chat) for chat in (CHAT).split()]
     LOG_GROUP=os.environ.get("LOG_GROUP", "")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
